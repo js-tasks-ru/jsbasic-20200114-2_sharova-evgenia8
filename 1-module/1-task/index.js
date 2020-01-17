88f147cc1b83087c82caf6838d824914bffa73bc
Evgenia Sharova <sharova.evgenia8@gmail.com>
2020-01-17 16:41:53 +0300

    добавлен 0 и 1 модуль(2 из 5)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,12 @@
  * @returns {number}
  */
 function pow(m, n) {
-
+  let result = 1;
+  for (let i = 0; i < n; i++) {
+    result *= m;
+  }
+  return result;
 }
+let m = parseInt(prompt('Введите m', ''));
+let n = parseInt(prompt('Введите n', ''));
+alert(pow(m, n));
