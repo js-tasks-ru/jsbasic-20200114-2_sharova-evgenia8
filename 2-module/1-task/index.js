04bf910427b4a9ae8db5f26215be2b387e716276
Evgenia Sharova <sharova.evgenia8@gmail.com>
2020-01-22 17:19:51 +0300

    сумма зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,10 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-
+  let salaryAmount = 0;
+  for (key in salaries) {
+    if (typeof salaries[key] == 'number') {
+      salaryAmount += salaries[key];
+    }
+  } return salaryAmount;
 }
