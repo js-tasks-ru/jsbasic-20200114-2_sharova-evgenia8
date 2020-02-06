c10791161d7e0ebef83be0fbbff0405fd2fe5205
Evgenia Sharova <sharova.evgenia8@gmail.com>
2020-02-06 20:07:24 +0300

    min/max

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,16 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+function getMinMax(string) {
+  const arr = string.split(",")
+    .join(' ')
+    .split(' ')
+    .map(item => +item)
+    .filter(item => !isNaN(item))
+    .sort((a, b) => a - b)
+  return {
+    min: arr[0],
+    max: arr[arr.length - 1]
+  }
 }
+
